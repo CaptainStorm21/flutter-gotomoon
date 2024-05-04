fc0ace3e9bf6d2e22e1b6f54a9c085e11237b8f6
Captain Storm <CaptainStorm21@users.noreply.github.com>
2024-05-03 23:57:44 -0700

    Text - decoration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,13 +6,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
+      body: _pageTitle(),
+    );
+  }
+
+  Widget _pageTitle(){
+    return const Text("#GoMoon",
+    style: TextStyle(
+      color: Colors.cyanAccent,
+      fontSize: 40,
+      fontWeight: FontWeight.w300,
+
+    )
+    );
+
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
           image: AssetImage("assets/images/supermoon.jpg"),
           fit: BoxFit.cover,
         ),
       ),
-    ));
+    );
   }
 }
